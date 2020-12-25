1c1f4375fe286bea30f71a33ab2d1f4b5063b578
Admin <admin@MacBook-Pro-Admin-2.local>
2020-12-25 20:28:34 +0200

    first git

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const config = require("config");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const PORT = config.get("port") || 5000;
 const app = express();
 
 app.use(express.json({ extended: true }));
 
-// app.use("/api/auth/", require("./routes/auth.routes"));
-// app.use("/api/link/", require("./routes/link.routes"));
-// app.use("/t/", require("./routes/redirect.routes"));
+app.use("/api/auth/", require("./routes/auth.routes"));
+app.use("/api/link/", require("./routes/link.routes"));
+app.use("/t/", require("./routes/redirect.routes"));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use("/", express.static(path.join(__dirname, "client", "build")));
-//
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = config.get("port") || 5000;
 
 async function start() {
   try {
